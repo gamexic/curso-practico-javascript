b65e013ba4860161f4eb364ec8cebef8efa68260
Frank Alayza <stephanoherrera4444@gmail.com>
2022-03-10 02:28:04 -0500

    Agregamos los cupones en HTML y JS

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,3 @@
-const precioOriginal = 100;
-const descuento = 15;
-
 
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -23,9 +20,50 @@ function onClickButtonPriceDiscount() {
 }
 
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// })
+function onCliclButtonCouponDiscount() {
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount")
+    const discountValue = inputDiscount.value;
+
+    function discountToDo(price, discountt) {
+        const discount = (discountt * price) / 100;
+    
+        return discount;
+    }
+
+    const coupons = [
+        {
+            name: "FrankIsBatman",
+            discount: 15,
+        },
+        {
+            name: "DoNotTellAnyone",
+            discount: 30,
+        },
+        {
+            name: "ItIsASecret",
+            discount: 25,
+        },
+    ]
+
+    const isCouponValueValid = function (coupons) {
+        return coupons.name === couponValue;
+    }
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + "no es válido");
+    } else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = (calcularPrecioConDescuento(priceValue, descuento)) - discountToDo(priceValue, discountValue);
+
+        const resultPrice = document.getElementById("ResultPrice");
+        resultPrice.innerText = "El precio con descuento son: s/." + precioConDescuento;
+    }
+}
